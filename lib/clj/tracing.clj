bf7eafcd3f32dbd02870769b2d23c4b6436a6407
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-09 18:29:45 -0200

    Tracing as string (for now)

--- lib/clj/tracing.clj
+++ lib/clj/tracing.clj
@@ -6,3 +6,22 @@
     (sayid/ws-reset!)
     (doseq [f fn] (sayid/ws-add-trace-fn!* f))
     (doseq [n ns] (sayid/ws-add-trace-ns!* n))))
+
+(defn- truncate [obj]
+  (let [s (str obj)]
+    (if (< (count s) 20)
+      s
+      (-> s (subs 0 20) (str "...")))))
+
+(defn- trace-child [children]
+  (when (not-empty children)
+    (for [child children]
+      {:fn (str (some-> child :meta :ns .name) (some-> child :meta :fn))
+       :args (mapv truncate (:args child))
+       :returned (:return child)
+       :children (trace-child @(:children child))})))
+
+(defn trace-str
+  ([] (trace-child (some-> (sayid/ws-get-active!) :children deref))))
+
+; (trace-str)
