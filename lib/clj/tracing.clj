c0c9d26018305f9bb9008cd7b10e90f1fc2983a8
Maurício Szabo <mauricio.szabo@gmail.com>
2016-12-18 10:38:16 -0200

    Changes on tracing

--- lib/clj/tracing.clj
+++ lib/clj/tracing.clj
@@ -32,30 +32,52 @@
                 :children (-> outer-fns (concat (trace-child child)) vec)}]
     [let-fn]))
 
-(defn- function [child]
-  (println child)
-  [{:id (name (:id child))
-    :fn (str (or #_(:form child) (:name child)))
-    :args (not-empty (mapv truncate (:args child)))
-    :mapping (some->> child :arg-map deref not-empty (map (fn [[k v]] [k (str v)])) (into {}))
-    :returned (str (:return child))
-    :children (trace-child child)}])
+(defn- treat-return [return]
+  (str (if (seq? return)
+         (seq return)
+         return)))
 
+(defn- function [child]
+  ; (println "BUA2 =>" (select-keys child [:id :fn :src-pos :xpanded-frm :xpanded-parent :form :arg-map :args :mapping :return]))
+  (let [f (str (or (:form child) (:name child) (:xpanded-frm child)))]
+    ; (if (or (.startsWith f "(fn*") (.startsWith f "(let*"))
+    ;   (trace-child child)
+    [{:id (name (:id child))
+      :fn f
+      :args (not-empty (mapv treat-return (:args child)))
+      :mapping (some->> child :arg-map deref not-empty (map (fn [[k v]] [k (str v)])) (into {}))
+      :returned (treat-return (:result child))
+      :children (trace-child child)}]))
+
+(defn p [x] (println "DEBUG:" x) x)
 (defn- trace-child [workspace]
+  ; (println "BUA2" (select-keys workspace [:name :form :src-pos :xpanded-form :pos]))
   (when-let [children (some-> workspace :children deref not-empty)]
     (mapcat (fn [child]
+              (println "BUA" (select-keys child [:name :form :xpanded-frm :id]))
+              ; (println "KEYS" (keys child))
               (cond
                 (:let-binds child) (let-binds child)
-                (and (:form child) (-> child :src-pos empty?)) (trace-child child)
-                :else (function child)))
+
+                ; (or (some-> child :name p (not= 'let*))
+                ;     (and (p (:form child)) #_(-> child :form first p (not= 'let*))))
+                :all
+                (function child)
+
+                :else (trace-child child)))
             children)))
+
+            ;  (and (-> child :name nil?) (-> child :form nil?) (-> child :xpanded-frm nil?) (-> child :src-pos empty?))
+            ;     ; (-> child :src-pos empty?))
+            ;  (trace-child child)
+             ;
+            ;  :else (function child)))
 
 (defn trace-str []
   (trace-child (sayid/ws-get-active!)))
 
 (defn trace-inner [id]
   (when-let [call (-> [:id id] sayid/ws-query :children first)]
-    (clojure.pprint/pprint call)
     (let [var (ns-resolve (or (:ns call) *ns*) (:name call))
           f (symbol (str (.name (.ns var)) "/" (.sym var)))
           args (:args call)]
