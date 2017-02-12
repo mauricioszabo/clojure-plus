a182c9cb6c743413fbd0f1d03afda89f8a92d89c
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-12 21:14:28 -0200

    Removed comments

--- src/clojure_plus/core.cljs
+++ src/clojure_plus/core.cljs
@@ -2,16 +2,8 @@
   (:require ;[clojure.browser.repl :as repl]
             [cljs.nodejs :as nodejs]
             [weasel.repl :as repl]))
-;             [clojure.browser.repl :as repl]))
-;
 
-; (def vm (js/require "vm"))
-; (defn eval-code [code]
-;   ())
-
-(+ 1 2)
-
-(def vm (js/require "vm"))
+(-> js/atom .-notifications (.addSuccess "TESTE!"))
 
 (defmethod repl/process-message :eval-js [message]
   (let [code (:code message)]
@@ -37,19 +29,3 @@
 
 (nodejs/enable-util-print!)
 (.log js/console "WOW!!!!!")
-
-; (println "Hello from the Node!")
-
-; (.log js/console
-;       "WOW DOIS!!!!!"
-;       (repl/connect "http://localhost:9000/repl"))
-;
-; (repl/connect "http://localhost:9000/repl")
-; (def -main (fn [] nil))
-; (set! *main-cli-fn* -main) ;; this is required
-
-; (ns clojure-plus.core
-;    (:require [clojure.browser.repl :as repl]))
-; ; Use of "localhost" will only work for local development.
-; ; Change the port to match the :repl-listen-port.
-; (repl/connect "http://localhost:9000/repl")
