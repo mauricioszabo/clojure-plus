f3e74cfd30315ce2448a8a4ab6e9625f6abdbbb0
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-14 23:59:07 -0200

    Added compiler options, at last

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
                  [org.clojure/clojurescript "1.9.456"]
                 ;  [figwheel-sidecar "0.5.9"]
                  [com.cemerick/piggieback "0.2.1"]
-                 [weasel "0.7.0" :exclusions [org.clojure/clojurescript]]]
+                 [weasel "0.7.0" :exclusions [org.clojure/clojurescript]]
+                 [expectations "2.1.0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.9"]]
@@ -20,18 +21,15 @@
                         :compiler {;:main clojure-plus.core
                                    :output-to "lib/js/main.js"
                                    :output-dir "lib/js"
-                                  ;  :target :nodejs
+                                   ;  :target :nodejs
                                    :pretty-print true
-                                   :optimizations :simple}}]})
-                                  ;  :source-map true}}
-                      ;  {:id "figwheel-dev"
-                      ;   :source-paths ["src2"]
-                      ;   :figwheel true
-                      ;   :compiler {;:main clojure-plus.core
-                      ;              ;:main clojure-plus.initial
-                      ;              :output-to "lib/js2/main.js"
-                      ;              :output-dir "lib/js2"
-                      ;              :target :nodejs
-                      ;              :optimizations :none
-                      ;              :source-map true}}]}
-  ; :figwheel {})
+                                   :optimizations :simple}}
+                       {:id "dev"
+                        :source-paths ["src" "test"]
+                        :compiler {
+                                   :main clojure-plus.helpers-test
+                                   :output-to "lib/js/main.js"
+                                   :target :nodejs
+                                   :optimizations :simple
+                                   :output-wrapper true
+                                   :pretty-print true}}]})
