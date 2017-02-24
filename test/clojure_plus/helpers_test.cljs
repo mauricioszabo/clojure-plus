b3830c33b566fe135c3a7a4e95bd665969d3341d
Maurício Szabo <mauricio.szabo@gmail.com>
2017-02-24 10:36:03 -0300

    Testing tests

--- test/clojure_plus/helpers_test.cljs
+++ test/clojure_plus/helpers_test.cljs
@@ -1,7 +1,9 @@
 (ns ^:figwheel-always clojure-plus.helpers-test
-  (:require-macros [clojure.test :refer [testing is]])
-  (:require ;[clojure-plus.test :refer [testing]
+  ; (:require-macros [clojure.test :refer [deftest testing is run-tests]])
+  (:require ;[clojure-plus.test :refer [run-tests]]
             ; [cljs.test :as t]
+            [cljs.test :refer-macros [deftest is testing run-tests]]
+
             [clojure-plus.helpers :as helpers]
             [clojure-plus.core :as core]))
 
@@ -14,8 +16,14 @@
       js->clj
       (->> (filter #(= (% "displayName") name)))))
 
-; (testing "FooBar"
-;   (is (= "Foo" "Bar")))
+(deftest foobar
+  (testing "FooBar"
+    (is (= "Foo" "Foo"))))
+
+
+(set! js/__dirname (str (.resolve (js/require "path") ".")
+                        "/lib/js/foo/bar"))
+(run-tests)
 ;
 ; (do ; Add commands
 ;   (helpers/add-command "foo" "Sample Test" #(println))
