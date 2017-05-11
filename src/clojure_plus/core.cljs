2758ef2656490a264a9a3a723e5ded11501a1cdc
Maurício Szabo <mauricio.szabo@gmail.com>
2017-05-11 19:18:35 -0300

    Put REPL into repl namespace.

--- src/clojure_plus/core.cljs
+++ src/clojure_plus/core.cljs
@@ -1,17 +1,9 @@
 (ns clojure-plus.core
-  (:require [cljs.nodejs :as nodejs]
-            [figwheel.client.utils :as fig.utils]
-            [cljs.reader :as edn]))
-            ; [weasel.repl :as repl]))
+  (:require [cljs.nodejs :as nodejs]))
 
 (defonce disposable
   (-> js/window (aget "clojure plus extensions") .-disposable))
 
-; (set! (.-exports js/module)
-;   #js {:foo "BAR"})
-; (set! (-> js/module .-exports)
-;       #js {:disposable disposable})
-;
 (nodejs/enable-util-print!)
 
 (defn command-for [name f]
@@ -19,15 +11,3 @@
                                           (str "clojure-plus:" name)
                                           f))]
     (.add disposable disp)))
-
-(def repl (-> (js/require "../../clojure-plus")
-              .getCommands .-promisedRepl))
-
-(defn execute-cmd
-  ([cmd callback] (execute-cmd cmd {} callback))
-  ([cmd ns-or-opts callback]
-   (-> repl
-       (.syncRun (str cmd) ns-or-opts)
-       (.then #(if-let [val (.-value %)]
-                 (callback {:value (edn/read-string val)})
-                 (callback {:error (or (.-error %) %)}))))))
